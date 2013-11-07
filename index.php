eb4b82fe520dc460b19a6a879a12c4cb80a06c36
fanqihit <1430988405@qq.com>
2013-11-07 15:58:32 +0800

    index-1

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf8">
+<title>么么淘--http://memetao.hit.edu.cn</title>
+<link type="text/css" rel="stylesheet" href="frame.css">
+<style>
+	a.help:link {color:#ff0000;text-decoration:none}
+	a.help:visited {color:#0000ff;text-decoration:none}
+	a.help:hover {color:#ffcc00;text-decoration:none}
+	a.help:active {color:#008000;text-decoration:none}
+</style>
+</head>
+<body>
+<div id="modular1">
+	<div id="help">
+		<p><a  class="help" href="help.php" target=_blank  title="点击获得帮助">帮助</a></p>
+	</div>
+	<div id="login">
+		<p align="center">登录</p>
+	</div>	
+</div>
+<div id="modular2">
+	<div id="memetao">
+		<img src="memetao.jpg" align="center" width="200px" height="100px" alt="logo加载中"/>
+	</div>
+	<div id="search">
+		<p align="center">搜索框</p>
+	</div>
+	<div id="release">
+		<p align="center">发布</p>
+	</div>
+</div>
+<div id="modular3">
+	<div id="goodsnews">
+		<p align="center">商品分类</p>
+	</div>
+	<div id="tag">
+		<p align="center">xxx</p>
+	</div>
+</div>
+<div id="modular4">
+	<div id="newgoods">
+		<p align="center">今日新品</p>
+	</div>
+	<div id="discount">
+		<p align="center">聚划算</p>
+	</div>
+	<div id="shopping">
+		<p align="center">特色购物</p>
+	</div>
+	<div id="myneed">
+		<p align="center">需求空间</p>
+	</div>
+</div>
+<div id="modular5">
+	<div id="link">
+		<p align="right">校内链接</p>
+	</div>
+</div>
+</body>
+</html>
