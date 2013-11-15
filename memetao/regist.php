50111b4870ada1a3fb25f682a2de8e7b00b1bfba
fanqihit <1430988405@qq.com>
2013-11-15 21:24:01 +0800

    memetao

---
+++ memetao/regist.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+<title>会员注册系统</title>
+</head>
+<body>
+<h1 align="center">么么淘会员注册</h1>
+<form name="form1" action="Insert.php" method="post">
+	<p align="center">邮箱  <input type="text" name="user_email" size="20" maxlength="55"></p>
+	<p align="center">用户名<input type="text" name="user_name" size="20"></p>
+	<p align="center">密码  <input type="password" name="user_password" size="20"></p>
+	<p align="center">确认密码  <input type="password" name="user_password" size="20"></p>
+	<p align="center"><input type="submit" name="提交"></input><input type="reset" name="重置"></input></p>
+</form>
+</body>
+</html>
